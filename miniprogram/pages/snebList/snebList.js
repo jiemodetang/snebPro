d5cb1cca3f1a3b0d6a2284bcf2741f29d6782f98
wentai.zhou <943990462@qq.com>
2019-05-08 11:51:32 +0800

    npm帶修改

--- miniprogram/pages/snebList/snebList.js
+++ miniprogram/pages/snebList/snebList.js
@@ -1,102 +1,121 @@
-let col1H = 0;
-let col2H = 0;
-
 Page({
 
+  /**
+   * 页面的初始数据
+   */
   data: {
-    scrollH: 0,
-    imgWidth: 0,
-    loadingCount: 0,
-    images: [],
-    col1: [],
-    col2: []
+    films: [],
+    fromDataBase: [],
+    hasMore: true,
+    start: 0
   },
 
-  onLoad: function () {
-    wx.getSystemInfo({
-      success: (res) => {
-        let ww = res.windowWidth;
-        let wh = res.windowHeight;
-        let imgWidth = ww * 0.48;
-        let scrollH = wh;
-
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+    this.getSnebListDate()
+  },
+  getSnebListDate: function() {
+    //还要修改的storge的逻辑
+    
+    const db = wx.cloud.database()
+    // 查询当前用户所有的 snebClassBsdSnebInfor
+    db.collection('snebClassBsdSnebInfor').where({
+      _id: this.data.id
+    }).get({
+      success: res => {
         this.setData({
-          scrollH: scrollH,
-          imgWidth: imgWidth
-        });
-
-        this.loadImages();
+          fromDataBase: res.data[0].snebList,
+          films: res.data[0].snebList
+        })
+        wx.setStorage({
+          key: 'snebClassBsdSnebInfor',
+          data: res.data[0].snebList,
+          success: function (res) {
+            console.log(res)
+            console.log('----设置缓存成功----')
+          }
+        })
+        console.log('[数据库] [查询记录] 成功: ')
+      },
+      fail: err => {
+        wx.showToast({
+          icon: 'none',
+          title: '查询记录失败'
+        })
+        console.error('[数据库] [查询记录] 失败：', err)
       }
     })
   },
 
-  onImageLoad: function (e) {
-    let imageId = e.currentTarget.id;
-    let oImgW = e.detail.width;         //图片原始宽度
-    let oImgH = e.detail.height;        //图片原始高度
-    let imgWidth = this.data.imgWidth;  //图片设置的宽度
-    let scale = imgWidth / oImgW;        //比例计算
-    let imgHeight = oImgH * scale;      //自适应高度
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
 
-    let images = this.data.images;
-    let imageObj = null;
-
-    for (let i = 0; i < images.length; i++) {
-      let img = images[i];
-      if (img.id === imageId) {
-        imageObj = img;
-        break;
-      }
-    }
-
-    imageObj.height = imgHeight;
-
-    let loadingCount = this.data.loadingCount - 1;
-    let col1 = this.data.col1;
-    let col2 = this.data.col2;
-
-    if (col1H <= col2H) {
-      col1H += imgHeight;
-      col1.push(imageObj);
-    } else {
-      col2H += imgHeight;
-      col2.push(imageObj);
-    }
-
-    let data = {
-      loadingCount: loadingCount,
-      col1: col1,
-      col2: col2
-    };
-
-    if (!loadingCount) {
-      data.images = [];
-    }
-
-    this.setData(data);
   },
 
-  loadImages: function () {
-    let images = [
-      { pic: 'http://t1.hxzdhn.com/uploads/tu/201807/9999/2cc7ab0bc5.jpg', height: 0 },
-      { pic: 'http://t1.hxzdhn.com/uploads/tu/201807/9999/f867c97e25.jpg', height: 0 },
-      { pic: 'http://t1.hxzdhn.com/uploads/tu/201807/9999/99495c4cf4.jpg', height: 0 },
-      { pic: 'http://t2.hddhhn.com/uploads/tu/201707/521/83.jpg', height: 0 },
-      { pic: 'http://t1.hxzdhn.com/uploads/tu/201807/9999/f867c97e25.jpg', height: 0 },
-      { pic: 'http://t1.hxzdhn.com/uploads/tu/201807/9999/99495c4cf4.jpg', height: 0 },
-      { pic: 'http://t2.hddhhn.com/uploads/tu/201707/521/83.jpg', height: 0 },
-    ];
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
 
-    let baseId = "img-" + (+new Date());
+  },
 
-    for (let i = 0; i < images.length; i++) {
-      images[i].id = baseId + "-" + i;
-    }
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
 
-    this.setData({
-      loadingCount: images.length,
-      images: images
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {
+
+  },
+  onPullDownRefresh: function() {
+    wx.showToast({
+      title: '数据加载中',
+      icon: 'loading',
+      duration: 1000
     });
-  }
-
+    var that = this
+    this.getSnebListDate()
+  },
+  onReachBottom: function() {
+    var that = this
+    that.setData({
+      films: [...this.data.films, ...this.data.fromDataBase]
+    })
+  },
+  goSnebDetail: function(e) {
+    var data = e.currentTarget.dataset;
+    wx.navigateTo({
+      url: "../snebDetail/snebDetail?id=" + data.id
+    })
+  },
 })
--- miniprogram/pages/snebMe/snebMe.js
+++ miniprogram/pages/snebMe/snebMe.js
@@ -1,66 +1,142 @@
 // miniprogram/pages/snebMe/snebMe.js
+const app = getApp()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    avatarUrl: './user-unlogin.png',
+    userInfo: {},
+    userNickName: '',
+    logged: false,
+    takeSession: false,
+    requestResult: '',
+    grids: [{ "gridSelect": 'class' }, { "gridSelect": '2' }, { "gridSelect": '2' }, { "gridSelect": '2' }, { "gridSelect": '2' }, { "gridSelect": '2' }, { "gridSelect": '2' }]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
+    if (!wx.cloud) {
+      wx.redirectTo({
+        url: '../chooseLib/chooseLib',
+      })
+      return
+    }
 
+    // 获取用户信息
+    wx.getSetting({
+      success: res => {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+          wx.getUserInfo({
+            success: res => {
+              this.setData({
+                avatarUrl: res.userInfo.avatarUrl,
+                userInfo: res.userInfo,
+                userNickName: res.userInfo.nickName
+              })
+            }
+          })
+        }
+      }
+    })
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
+    setTimeout(() => {
+      console.log(this.data.userInfo)
+    }, 2000)
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
-  }
+  },
+  toLogin: function() {
+    // 调用云函数
+    wx.cloud.callFunction({
+      name: 'login',
+      data: {},
+      success: res => {
+        console.log('[云函数] [login] user openid: ', res.result.openid)
+        app.globalData.openid = res.result.openid
+        // wx.navigateTo({
+        //   url: '../userConsole/userConsole',
+        // })
+      },
+      fail: err => {
+        console.error('[云函数] [login] 调用失败', err)
+        wx.navigateTo({
+          url: '../deployFunctions/deployFunctions',
+        })
+      }
+    })
+
+    // const db = wx.cloud.database()
+    // // 查询当前用户所有的 snebBanJi
+    // db.collection('snebBanJi').where({
+
+    //   _id: this.data.id
+    // }).get({
+    //   success: res => {
+    //     console.log(res.data);
+    //     // this.setData({
+    //     //   queryResult: JSON.stringify(res.data, null, 2)
+    //     // })
+    //     console.log('[数据库] [查询记录] 成功: ')
+    //   },
+    //   fail: err => {
+    //     wx.showToast({
+    //       icon: 'none',
+    //       title: '查询记录失败'
+    //     })
+    //     console.error('[数据库] [查询记录] 失败：', err)
+    //   }
+    // })
+  },
+
 })
